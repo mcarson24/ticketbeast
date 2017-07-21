728bf0905d2d21adb47acaaa5aeb9fee2270ea67
Matthew Carson <matthew24carson@gmail.com>
2017-07-21 16:58:01 -0400

    Fix hashid alphabet
    Accidentially included 'X' twice instead of 'W'.

--- app/HashIdsTicketCodeGenerator.php
+++ app/HashIdsTicketCodeGenerator.php
@@ -10,7 +10,7 @@ class HashIdsTicketCodeGenerator implements TicketCodeGenerator
 
 	public function __construct($salt)
 	{
-		$this->hashids = new Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVXXYZ');
+		$this->hashids = new Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 	}
 
 	public function generateFor(Ticket $ticket)

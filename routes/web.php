eddd9fad8ded075b60b0c99c66d8cddce789ca1c
Matthew Carson <matthew24carson@gmail.com>
2017-07-27 14:34:35 -0400

    Dusk is all good to go.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::get('/', function() {
+	return 'Laravel';
+});
+
 Route::get('concerts/{id}', 'ConcertsController@show');
 Route::post('concerts/{id}/orders', 'ConcertOrdersController@store');
 Route::get('orders/{confirmation_number}', 'OrdersController@show');

99699824a0851de5a24e0cdfcd27740f559b6cbc
Matthew Carson <matthew24carson@gmail.com>
2017-07-27 15:39:56 -0400

    Prevent groups from viewing add concerts form

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('concerts/{id}', 'ConcertsController@show');
 Route::post('concerts/{id}/orders', 'ConcertOrdersController@store');
 Route::get('orders/{confirmation_number}', 'OrdersController@show');
 
-Route::get('backstage/concerts/new', 'Backstage\ConcertsController@create');
-
 Route::get('login', 'Auth\LoginController@showLoginForm');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('auth.logout');
+
+Route::group(['middleware' => 'auth'], function() {
+	Route::get('backstage/concerts/new', 'Backstage\ConcertsController@create');
+});

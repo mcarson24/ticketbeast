cd3765d757431b2867bf859c20a5f4bd8439405f
Matthew Carson <matthew24carson@gmail.com>
2017-07-27 15:33:02 -0400

    Add concert creation page

---
+++ resources/views/layouts/backstage.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('body')
+<div class="full-height bg-soft flex-col">
+    <header>
+        <nav class="navbar p-xs-y-3">
+            <div class="container">
+                <div class="navbar-content">
+                    <div>
+                        <img src="/img/logo.svg" alt="TicketBeast" style="height: 2.5rem;">
+                    </div>
+                    <div>
+                        <form class="inline-block" action="{{ route('auth.logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            <button type="submit" class="link link-light">Log out</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="flex-fit">
+        @yield('backstageContent')
+    </div>
+
+    <footer class="bg-dark p-xs-y-6 text-light-muted">
+        <div class="container">
+            <p class="text-center">&copy; TicketBeast {{ date('Y') }}</p>
+        </div>
+    </footer>
+</div>
+@endsection

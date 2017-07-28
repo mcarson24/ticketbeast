017507706ac437cfc48a0c322802de1d4415673b
Matthew Carson <matthew24carson@gmail.com>
2017-07-28 15:23:07 -0400

    Auto publish concerts on creation

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -69,6 +69,18 @@ class ConcertTest extends TestCase
     }
 
     /** @test */
+    public function concerts_can_be_published()
+    {
+        $concert = factory(Concert::class)->states('unpublished')->create();
+
+        $this->assertFalse($concert->isPublished());
+        
+        $concert->publish();
+
+        $this->assertTrue($concert->isPublished());
+    }
+
+    /** @test */
     public function can_add_tickets()
     {
         $concert = factory(Concert::class)->create();

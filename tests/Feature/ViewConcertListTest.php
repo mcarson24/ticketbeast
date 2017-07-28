df6b492ab2cf104818a0ffecdf6e6bd7e167015d
Matthew Carson <matthew24carson@gmail.com>
2017-07-28 18:06:56 -0400

    Refactor ViewConcertTest assertions with macros

--- tests/Feature/ViewConcertListTest.php
+++ tests/Feature/ViewConcertListTest.php
@@ -5,11 +5,31 @@ namespace Tests\Feature;
 use App\User;
 use App\Concert;
 use Tests\TestCase;
+use PHPUnit\Framework\Assert;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ViewConcertListTest extends TestCase
 {
 	use DatabaseMigrations;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		TestResponse::macro('data', function($key) {
+			return $this->original->getData()[$key];
+		});
+
+		Collection::macro('assertContains', function($value) {
+			return Assert::assertTrue($this->contains($value), "Failed asserting that the collection contained:\n{$value}");
+		});
+
+		Collection::macro('assertNotContains', function($value) {
+			return Assert::assertFalse($this->contains($value), "The collection contained an unwanted value:\n{$value}");
+		});
+	}
 
     /** @test */
     public function guests_cannot_view_a_promoters_concert_list()
@@ -33,9 +53,9 @@ class ViewConcertListTest extends TestCase
         $response = $this->actingAs($user)->get('backstage/concerts');
 
         $response->assertStatus(200);
-        $this->assertTrue($response->original->getData()['concerts']->contains($concertA));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concertB));
-        $this->assertFalse($response->original->getData()['concerts']->contains($concertC));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concertD));
+        $response->data('concerts')->assertContains($concertA);
+        $response->data('concerts')->assertContains($concertB);
+        $response->data('concerts')->assertContains($concertD);
+        $response->data('concerts')->assertNotContains($concertC);
     }
 }

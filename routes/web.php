14638171161ab4adca64598c53ab1d8c51070b91
Matthew Carson <matthew24carson@gmail.com>
2017-08-02 14:57:24 -0400

    Fix route names mix up

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::post('logout', 'Auth\LoginController@logout')->name('auth.logout');
 
 Route::group(['middleware' => 'auth', 'prefix' => 'backstage', 'namespace' => 'Backstage'], function() {
 	Route::get('concerts', 'ConcertsController@index')->name('backstage.concerts.index');
-	Route::post('concerts', 'ConcertsController@store')->name('backstage.concerts.new');
-	Route::get('concerts/new', 'ConcertsController@create');
+	Route::post('concerts', 'ConcertsController@store');
+	Route::get('concerts/new', 'ConcertsController@create')->name('backstage.concerts.new');
 	Route::get('concerts/{id}/edit', 'ConcertsController@edit')->name('backstage.concerts.edit');
 	Route::patch('concerts/{id}', 'ConcertsController@update')->name('backstage.concerts.update');
 });

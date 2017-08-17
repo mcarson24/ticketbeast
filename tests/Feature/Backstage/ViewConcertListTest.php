59ea06e19f41a764e6579902ce798fe1b56c926f
Matthew Carson <matthew24carson@gmail.com>
2017-08-17 16:19:07 -0400

    Move collection macros into base TestCase

--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -5,36 +5,11 @@ namespace Tests\Feature;
 use App\User;
 use App\Concert;
 use Tests\TestCase;
-use ConcertFactory;
-use PHPUnit\Framework\Assert;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ViewConcertListTest extends TestCase
 {
 	use DatabaseMigrations;
-
-	public function setUp()
-	{
-		parent::setUp();
-
-		Collection::macro('assertContains', function($value) {
-			return Assert::assertTrue($this->contains($value), "Failed asserting that the collection contained:\n{$value}");
-		});
-
-		Collection::macro('assertNotContains', function($value) {
-			return Assert::assertFalse($this->contains($value), "The collection contained an unwanted value:\n{$value}");
-		});
-
-        Collection::macro('assertEquals', function($items) {
-            Assert::assertEquals(count($this), count($items));
-
-            $this->zip($items)->each(function($pair) {
-                list($a, $b) = $pair;
-                Assert::assertTrue($a->is($b));
-            });
-        });
-	}
 
     /** @test */
     public function guests_cannot_view_a_promoters_concert_list()
@@ -51,13 +26,13 @@ class ViewConcertListTest extends TestCase
         $user = factory(User::class)->create();
         $otherUser = factory(User::class)->create();
 
-        $publishedConcertA = ConcertFactory::createPublished(['user_id' => $user->id]);
-        $publishedConcertB = ConcertFactory::createPublished(['user_id' => $user->id]);
-        $publishedConcertC = ConcertFactory::createPublished(['user_id' => $otherUser->id]);
+        $publishedConcertA = \ConcertFactory::createPublished(['user_id' => $user->id]);
+        $publishedConcertB = \ConcertFactory::createPublished(['user_id' => $user->id]);
+        $publishedConcertC = \ConcertFactory::createPublished(['user_id' => $otherUser->id]);
 
-        $unpublishedConcertA = ConcertFactory::createUnpublished(['user_id' => $user->id]);
-        $unpublishedConcertB = ConcertFactory::createUnpublished(['user_id' => $user->id]);
-        $unpublishedConcertC = ConcertFactory::createUnpublished(['user_id' => $otherUser->id]);
+        $unpublishedConcertA = \ConcertFactory::createUnpublished(['user_id' => $user->id]);
+        $unpublishedConcertB = \ConcertFactory::createUnpublished(['user_id' => $user->id]);
+        $unpublishedConcertC = \ConcertFactory::createUnpublished(['user_id' => $otherUser->id]);
 
         $response = $this->actingAs($user)->get('backstage/concerts');
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,12 @@ namespace Tests;
 use App\User;
 use \Mockery;
 use App\Exceptions\Handler;
+use PHPUnit\Framework\Assert;
 use Tests\CreatesApplication;
 use Illuminate\Foundation\Testing\TestResponse;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -22,6 +24,23 @@ abstract class TestCase extends BaseTestCase
 
         TestResponse::macro('data', function($key) {
             return $this->original->getData()[$key];
+        });
+
+        EloquentCollection::macro('assertContains', function($value) {
+            return Assert::assertTrue($this->contains($value), "Failed asserting that the collection contained:\n{$value}");
+        });
+
+        EloquentCollection::macro('assertNotContains', function($value) {
+            return Assert::assertFalse($this->contains($value), "The collection contained an unwanted value:\n{$value}");
+        });
+
+        EloquentCollection::macro('assertEquals', function($items) {
+            Assert::assertEquals(count($this), count($items));
+
+            $this->zip($items)->each(function($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
         });
     }
 

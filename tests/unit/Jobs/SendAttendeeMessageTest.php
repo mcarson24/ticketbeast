7125c45a360d6d094829af60d9bb3693303d615f
Matthew Carson <matthew24carson@gmail.com>
2017-08-23 17:28:04 -0400

    Complete sending of attendee message emails

---
+++ tests/unit/Jobs/SendAttendeeMessageTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use Tests\TestCase;
+use App\AttendeeMessage;
+use App\Jobs\SendAttendeeMessage;
+use App\Mail\AttendeeMessageEmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class SendAttendeeMessageTest extends TestCase
+{
+	use DatabaseMigrations;
+
+    /** @test */
+    public function it_sends_the_message_to_all_concert_attendees()
+    {
+    	Mail::fake();
+        $concert = \ConcertFactory::createPublished();
+        $otherConcert = \ConcertFactory::createPublished();
+        $message = AttendeeMessage::create([
+        	'concert_id' => $concert->id,
+        	'subject'    => 'My Subject',
+        	'message' 	 => 'My Message'
+    	]);
+    	$orderA = \OrderFactory::createForConcert($concert, ['email' => 'alex@example.com']);
+    	$orderForOtherConcert = \OrderFactory::createForConcert($otherConcert, ['email' => 'joe@example.com']);
+    	$orderB = \OrderFactory::createForConcert($concert, ['email' => 'sam@example.com']);
+    	$orderC = \OrderFactory::createForConcert($concert, ['email' => 'taylor@example.com']);
+
+    	SendAttendeeMessage::dispatch($message);
+
+    	Mail::assertSent(AttendeeMessageEmail::class, function($mail) use ($message) {
+    		return $mail->hasTo('alex@example.com') &&
+    			   $mail->attendeeMessage->is($message);
+    	});
+    	Mail::assertSent(AttendeeMessageEmail::class, function($mail) use ($message) {
+    		return $mail->hasTo('sam@example.com') &&
+    			   $mail->attendeeMessage->is($message);
+    	});
+    	Mail::assertSent(AttendeeMessageEmail::class, function($mail) use ($message) {
+    		return $mail->hasTo('taylor@example.com') &&
+    			   $mail->attendeeMessage->is($message);
+    	});
+    	Mail::assertNotSent(AttendeeMessageEmail::class, function($mail) use ($message) {
+    		return $mail->hasTo('joe@example.com');
+    	});
+    }
+}

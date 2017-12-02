d4f29db37be333647a994c33672b95d2c065e374
Matthew Carson <matthew24carson@gmail.com>
2017-12-01 23:59:09 -0500

    Resize poster image

---
+++ tests/Unit/Jobs/ProcessPosterImageTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Jobs;
+
+use Tests\TestCase;
+use App\Jobs\ProcessPosterImage;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProcessPosterImageTest extends TestCase
+{
+	use RefreshDatabase;
+
+    /** @test */
+    public function it_resizes_poster_image_to_600px_wide()
+    {
+    	Storage::fake('public');
+
+    	$concert = \ConcertFactory::createUnpublished([
+    		'poster_image_path' => 'posters/example-poster.png'
+    	]);
+
+    	Storage::disk('public')->put(
+    		'posters/example-poster.png',
+    		file_get_contents(base_path('tests/__fixtures__/full-size-poster.png'))
+    	);
+
+        ProcessPosterImage::dispatch($concert);
+
+        $resizedImage = Storage::disk('public')->get('posters/example-poster.png');
+        list($width) = getimagesizefromstring($resizedImage);
+
+        $this->assertEquals(600, $width);
+    }
+}

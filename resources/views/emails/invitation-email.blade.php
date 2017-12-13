015cdd99519fc5297ee20fdb2ad7c53cd2e4950e
Matthew Carson <matthew24carson@gmail.com>
2017-12-13 17:20:15 -0500

    Create concert invitation e,ail view

---
+++ resources/views/emails/invitation-email.blade.php
@@ -0,0 +1,5 @@
+<p>Hey we would like to invite you to promote your concerts on TicketBeast!</p>
+
+<p>Visit this link to register you account.</p>
+
+<a href="{{ url("invitations/{$invitation->code}") }}">{{ url("invitations/{$invitation->code}") }}</a>

94f7ac45320b975343751cccadde35a98f717305
Matthew Carson <matthew24carson@gmail.com>
2018-02-14 15:23:22 -0500

    I guess I forgot to set up the database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,17 +13,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Concert::class)->states('published')->create([
+        $faker = \Faker\Factory::create();
+        $gateway = new \App\Billing\FakePaymentGateway;
+
+        $user = factory(App\User::class)->create([
+            'email'     => 'person@example.com',
+            'password'  => bcrypt('secret')
+        ]);
+
+        $concert = \ConcertFactory::createPublished([
+            'user_id' => $user->id,
             'title' => "The Red Chord",
             'subtitle' => "with Animosity and Lethargy",
+            'additional_information' => "This concert is 19+.",
             'venue' => "The Mosh Pit",
             'venue_address' => "123 Example Lane",
             'city' => "Laraville",
             'state' => "ON",
             'zip' => "17916",
-            'date' => Carbon::parse('2016-12-13 8:00pm'),
+            'date' => Carbon::today()->addMonths(3)->hour(20),
             'ticket_price' => 3250,
-            'additional_information' => "This concert is 19+.",
-        ])->addTickets(10);
+            'ticket_quantity' => 225
+        ]);
+
+        foreach(range(1, 50) as $i) {
+            Carbon::setTestNow(Carbon::instance($faker->dateTimebetween('-2 months')));
+
+            $concert->reserveTickets(rand(1, 4), $faker->safeEmail)
+                    ->complete($gateway, $gateway->getValidTestToken($faker->creditCardNumber));
+        }
+
+        Carbon::setTestNow();
+
+        factory(App\Concert::class)->create([
+            'user_id' => $user->id,
+            'title' => "Slayer",
+            'subtitle' => "with Forbidden and Testament",
+            'additional_information' => null,
+            'venue' => "The Rock Pile",
+            'venue_address' => "55 Sample Blvd",
+            'city' => "Laraville",
+            'state' => "ON",
+            'zip' => "19276",
+            'date' => Carbon::today()->addMonths(4)->hour(18),
+            'ticket_price' => 5500,
+            'ticket_quantity' => 10,
+        ]);
     }
 }
